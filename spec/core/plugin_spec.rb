b3c93dccb9babe5225e4ef19a0890b3fe13878d8
MATSUOKA Kohei <kohei@machu.jp>
2012-08-28 01:07:28 +0900

    add spec

--- spec/core/plugin_spec.rb
+++ spec/core/plugin_spec.rb
@@ -20,29 +20,38 @@ describe TDiary::Plugin do
 		end
 
 		it 'プラグイン一覧が @plugin_files で取得できること' do
+			# TODO: 実際にはPlugin.newした時点でload_pluginが呼ばれている
+			# @plugin_filesの追加もinitializeメソッド内で実行されている
 			subject.instance_variable_get(:@plugin_files).should include('spec/fixtures/plugin/sample.rb')
 		end
 
 		context 'リソースファイルが存在する場合' do
-			before { pending 'To be written' }
+			before do
+				@plugin.instance_variable_get(:@conf).lang = 'ja'
+				@plugin.load_plugin('spec/fixtures/plugin/sample.rb')
+			end
 
-			it 'Confファイルで指定した言語に対応するリソースが読み込まれること'
+			it 'Confファイルで指定した言語に対応するリソースが読み込まれること' do
+				@plugin.sample_ja.should eq 'サンプルプラグイン'
+			end
 		end
 	end
 
 	describe '#eval_src' do
-		before { @src = ERB::new('hello <%= sample %><%= undefined_method %>').src }
+		before do
+			@src = ERB::new('hello <%= sample %><%= undefined_method %>').src
+			@plugin.instance_variable_set(:@debug, false)
+		end
 		subject { @plugin.eval_src(@src, false) }
 
-		context 'debugモードがOFFの場合' do
-			before { @plugin.instance_variable_set(:@debug, false) }
+		it 'Pluginオブジェクト内でソースが実行されること' do
+			should eq 'hello sample plugin'
+		end
 
-			it 'Pluginオブジェクト内でソースが実行されること' do
-				subject.should eq 'hello sample plugin'
-			end
-
-			context 'secureモード指定の場合' do
-				it 'Safeモード4で実行されること'
+		context 'secureモード指定の場合' do
+			it 'Safeモード4で実行されること' do
+				Safe.should_receive(:safe).with(4)
+				@plugin.eval_src(@src, true)
 			end
 		end
 
@@ -86,7 +95,7 @@ describe TDiary::Plugin do
 			@plugin.__send__(:add_update_proc, proc1)
 			@plugin.__send__(:add_update_proc, proc2)
 		end
-		subject { @plugin.__send__(:update_proc, nil, nil) }
+		subject { @plugin.__send__(:update_proc) }
 
 		it 'add_update_procで登録したブロックが実行されること' do
 			proc1.should_receive(:call)
@@ -101,59 +110,217 @@ describe TDiary::Plugin do
 	end
 
 	describe '#title_proc' do
-		let (:proc1) { lambda {|date, title| "#{title}:title1" } }
-		let (:proc2) { lambda {|date, title| "#{title}:title2" } }
+		let (:proc1) { lambda {|date, title| "title1" } }
+		let (:proc2) { lambda {|date, title| "title2" } }
+		let (:date) { Time.local(2012, 1, 2) }
 		before do
 			@plugin.__send__(:add_title_proc, proc1)
 			@plugin.__send__(:add_title_proc, proc2)
 		end
-		subject { @plugin.__send__(:title_proc, nil, nil) }
+		subject { @plugin.__send__(:title_proc, date, 'title') }
 
-		it 'add_title_procで登録したブロックを実行し、タイトル文字列を返すこと' do
-			should eq ':title1:title2'
+		it 'add_title_procで登録したブロックを実行し、最後の結果を返すこと' do
+			should eq 'title2'
+		end
+
+		it '前のprocの結果が次のprocに渡されること' do
+			proc1.should_receive(:call).with(date, 'title').and_return('title1')
+			proc2.should_receive(:call).with(date, 'title1')
+			@plugin.__send__(:title_proc, date, 'title')
 		end
 
 		it 'apply_pluginメソッドを呼び出すこと' do
 			@plugin.should_receive(:apply_plugin)
 			# should_receiveの場合はsubjectが使えないため明示的に実行
-			@plugin.__send__(:title_proc, nil, nil)
+			@plugin.__send__(:title_proc, date, 'title')
 		end
 	end
 
 	describe '#body_enter_proc' do
-		it 'add_body_enter_procで登録したブロックが実行されること'
+		let (:date) { Time.local(2012, 1, 2) }
+		before do
+			@plugin.__send__(:add_body_enter_proc, lambda {|date| 'body1 ' })
+			@plugin.__send__(:add_body_enter_proc, lambda {|date| 'body2' })
+		end
+		subject { @plugin.__send__(:body_enter_proc, date) }
+
+		it 'add_body_enter_procで登録したブロックが実行されること' do
+			should eq 'body1 body2'
+		end
 	end
 
 	describe '#body_leave_proc' do
-		it 'add_body_leave_procで登録したブロックが実行されること'
+		before do
+			@plugin.__send__(:add_body_leave_proc, lambda {|date| 'body1 ' })
+			@plugin.__send__(:add_body_leave_proc, lambda {|date| 'body2' })
+		end
+		subject { @plugin.__send__(:body_leave_proc, @date) }
+
+		it 'add_body_leave_procで登録したブロックが実行されること' do
+			should eq 'body1 body2'
+		end
 	end
 
 	describe '#section_enter_proc' do
-		it 'add_section_enter_procで登録したブロックが実行されること'
+		let (:proc1) { lambda {|date, index| 'section1 ' } }
+		let (:proc2) { lambda {|date, index| 'section2' } }
+		let (:date1) { Time.local(2012, 1, 2) }
+		let (:date2) { Time.local(2012, 1, 3) }
+		before do
+			@plugin.__send__(:add_section_enter_proc, proc1)
+			@plugin.__send__(:add_section_enter_proc, proc2)
+		end
+		subject { @plugin.__send__(:section_enter_proc, date1) }
+
+		it 'add_section_enter_procで登録したブロックを実行し、結果を連結して返すこと' do
+			should eq 'section1 section2'
+		end
+
+		it '呼ばれた回数に応じてセクション番号の数が増加すること (日付単位)' do
+			proc1.should_receive(:call).with(date1, 1)
+			@plugin.__send__(:section_enter_proc, date1)
+			proc1.should_receive(:call).with(date1, 2)
+			@plugin.__send__(:section_enter_proc, date1)
+			proc1.should_receive(:call).with(date2, 1)
+			@plugin.__send__(:section_enter_proc, date2)
+		end
 	end
 
 	describe '#subtitle_proc' do
-		it 'add_subtitle_procで登録したブロックが実行されること'
+		let (:proc1) { lambda {|date, index, subtitle| "subtitle1" } }
+		let (:proc2) { lambda {|date, index, subtitle| "subtitle2" } }
+		let (:date1) { Time.local(2012, 1, 2) }
+		let (:date2) { Time.local(2012, 1, 3) }
+		before do
+			@plugin.__send__(:add_subtitle_proc, proc1)
+			@plugin.__send__(:add_subtitle_proc, proc2)
+		end
+		subject { @plugin.__send__(:subtitle_proc, date1, 'subtitle') }
+
+		it 'add_subtitle_procで登録したブロックを実行し、最後の結果を返すこと' do
+			should eq 'subtitle2'
+		end
+
+		it '前のprocの結果が次のprocに渡されること' do
+			proc1.should_receive(:call).with(date1, 1, 'subtitle').and_return('subtitle1')
+			proc2.should_receive(:call).with(date1, 1, 'subtitle1')
+			@plugin.__send__(:section_enter_proc, date1)
+			@plugin.__send__(:subtitle_proc, date1, 'subtitle')
+		end
+
+		it 'apply_pluginメソッドを呼び出すこと' do
+			@plugin.should_receive(:apply_plugin)
+			# should_receiveの場合はsubjectが使えないため明示的に実行
+			@plugin.__send__(:subtitle_proc, date1, 'title')
+		end
 	end
 
 	describe '#section_leave_proc' do
-		it 'add_section_leave_procで登録したブロックが実行されること'
+		let (:proc1) { lambda {|date, index| 'section1 ' } }
+		let (:proc2) { lambda {|date, index| 'section2' } }
+		let (:date1) { Time.local(2012, 1, 2) }
+		let (:date2) { Time.local(2012, 1, 3) }
+		before do
+			@plugin.__send__(:add_section_leave_proc, proc1)
+			@plugin.__send__(:add_section_leave_proc, proc2)
+		end
+		subject { @plugin.__send__(:section_leave_proc, date1) }
+
+		it 'add_section_leave_procで登録したブロックを実行し、結果を連結して返すこと' do
+			should eq 'section1 section2'
+		end
+
+		it '呼ばれた回数に応じてセクション番号の数が増加すること (日付単位)' do
+			# セクション番号はsection_enter_procの呼び出し回数で決定する
+			proc1.should_receive(:call).with(date1, 1)
+			@plugin.__send__(:section_enter_proc, date1)
+			@plugin.__send__(:section_leave_proc, date1)
+			proc1.should_receive(:call).with(date1, 2)
+			@plugin.__send__(:section_enter_proc, date1)
+			@plugin.__send__(:section_leave_proc, date1)
+			proc1.should_receive(:call).with(date2, 1)
+			@plugin.__send__(:section_enter_proc, date2)
+			@plugin.__send__(:section_leave_proc, date2)
+		end
 	end
 
 	describe '#comment_leave_proc' do
-		it 'add_comment_leave_procで登録したブロックが実行されること'
+		let (:date) { Time.local(2012, 1, 2) }
+		before do
+			@plugin.__send__(:add_comment_leave_proc, lambda {|date| 'comment1 ' })
+			@plugin.__send__(:add_comment_leave_proc, lambda {|date| 'comment2' })
+		end
+		subject { @plugin.__send__(:comment_leave_proc, date) }
+
+		it 'add_comment_leave_procで登録したブロックが実行されること' do
+			should eq 'comment1 comment2'
+		end
 	end
 
 	describe '#edit_proc' do
-		it 'add_edit_procで登録したブロックが実行されること'
+		let (:date) { Time.local(2012, 1, 2) }
+		before do
+			@plugin.__send__(:add_edit_proc, lambda {|date| 'edit1 ' })
+			@plugin.__send__(:add_edit_proc, lambda {|date| 'edit2' })
+		end
+		subject { @plugin.__send__(:edit_proc, date) }
+
+		it 'add_edit_procで登録したブロックが実行されること' do
+			should eq 'edit1 edit2'
+		end
 	end
 
 	describe '#form_proc' do
-		it 'add_form_procで登録したブロックが実行されること'
+		let (:date) { Time.local(2012, 1, 2) }
+		before do
+			@plugin.__send__(:add_form_proc, lambda {|date| 'form1 ' })
+			@plugin.__send__(:add_form_proc, lambda {|date| 'form2' })
+		end
+		subject { @plugin.__send__(:form_proc, date) }
+
+		it 'add_form_procで登録したブロックが実行されること' do
+			should eq 'form1 form2'
+		end
+	end
+
+	describe '#add_conf_proc' do
+		let(:proc) { lambda { 'conf' } }
+		subject { @plugin.__send__(:add_conf_proc, 'key1', 'label1', nil, proc) }
+
+		context '@modeがconfの場合' do
+			before { @plugin.instance_variable_set(:@mode, 'conf') }
+			it 'コールバックを登録すること' do
+				should include('label1')
+			end
+		end
+
+		context '@modeがsaveconfの場合' do
+			before { @plugin.instance_variable_set(:@mode, 'saveconf') }
+			it 'コールバックを登録すること' do
+				should include('label1')
+			end
+		end
+
+		context '@modeがconf, saveconf以外の場合' do
+			before { @plugin.instance_variable_set(:@mode, 'edit') }
+			it 'コールバックの登録を無視すること' do
+				should be_nil
+			end
+		end
 	end
 
 	describe '#conf_proc' do
-		it 'add_conf_procで登録したブロックが実行されること'
+		let (:date) { Time.local(2012, 1, 2) }
+		before do
+			@plugin.instance_variable_set(:@mode, 'conf')
+			@plugin.__send__(:add_conf_proc, 'key1', 'label1', nil, lambda { 'conf1' })
+			@plugin.__send__(:add_conf_proc, 'key2', 'label2', nil, lambda { 'conf2' })
+		end
+		subject { @plugin.__send__(:conf_proc, 'key1') }
+
+		it 'add_conf_procで登録したブロックのうち、keyが一致するものを実行して結果を返すこと' do
+			should eq 'conf1'
+		end
 	end
 
 	describe '#remove_tag' do
@@ -166,10 +333,30 @@ describe TDiary::Plugin do
 	end
 
 	describe '#apply_plugin' do
-		it 'プラグインが再実行されること'
+		before do
+			@plugin.instance_variable_get(:@conf).options['apply_plugin'] = true
+		end
+
+		subject { @plugin.__send__(:apply_plugin, '<%= sample %>') }
+		it 'プラグインが再実行されること' do
+			should eq 'sample plugin'
+		end
+
+		context '解釈できない文字列を渡された場合' do
+			subject { @plugin.__send__(:apply_plugin, '<%= undefined_method %>') }
+			it { should include 'Invalid Text' }
+		end
+
+		context '文字列がnilの場合' do
+			subject { @plugin.__send__(:apply_plugin, nil) }
+			it { should eq '' }
+		end
 
 		context 'remove_tagがtrueの場合' do
-			it 'remove_tagメソッドが呼び出されること'
+			it 'remove_tagメソッドを呼び出すこと' do
+				@plugin.should_receive(:remove_tag)
+				@plugin.__send__(:apply_plugin, '', true)
+			end
 		end
 	end
 end
