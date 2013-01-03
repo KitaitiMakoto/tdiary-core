5f875507dc54b9a2b2a4ef2aebda8093cce4210f
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-01-03 23:26:20 +0900

    Add Last-Modified header

--- tdiary/feed.rb
+++ tdiary/feed.rb
@@ -30,7 +30,7 @@ module TDiary
         end
       }
 
-      Response.new([feed.to_s], 200, {'Content-Type' => 'application/atom+xml'})
+      Response.new([feed.to_s], 200, {'Content-Type' => 'application/atom+xml', 'Last-Modified' => entries.first.last_modified.httpdate})
     end
 
     private
