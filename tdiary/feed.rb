847dd2db4eae3fc1aa450a9bae9f6cf0ccddaf43
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-01-03 23:14:53 +0900

    Extract method to aggregate entries

--- tdiary/feed.rb
+++ tdiary/feed.rb
@@ -2,50 +2,58 @@ require 'rss'
 
 module TDiary
   class Feed < Application
+    DEFAULT_COUNT = 15
+
     def call(env)
       request = adopt_rack_request_to_plain_old_tdiary_style(env)
       conf = Config.new(CGI.new, request)
       tdiary = TDiaryLatest.new(CGI.new, nil, conf)
-      io = conf.io_class.new(tdiary)
+      @io = conf.io_class.new(tdiary)
 
-      articles = []
-      count = 0
-      io.calendar.reverse_each do |year, months|
-        months.reverse_each do |month|
-          io.transaction(Date.new(year.to_i, month.to_i)) do |diaries|
-            diaries.reverse_each do |date, article|
-              if article.visible? and count < 15
-                articles << article
-                count += 1
-              end
-              TDiaryBase::DIRTY_DIARY
-            end
-          end
-          break if count >= 15
-        end
-        break if count >= 15
-      end
-
+      entries = entries(DEFAULT_COUNT)
       feed = RSS::Maker.make(@target) {|maker|
         maker.channel.about = conf['makeatom.url'] || "#{conf.base_url}recent.atom"
         maker.channel.title = conf.html_title
         maker.channel.author = conf.author_name
         maker.channel.description = conf.description || ''
         maker.channel.link = conf.base_url
-        maker.channel.updated = articles.first.last_modified
+        maker.channel.updated = entries.first.last_modified
 
-        articles.each do |article|
+        entries.each do |entry|
           maker.items.new_item do |item|
-            item.link = "#{conf.base_url}?date=#{article.date.strftime('%Y%m%d')}"
-            item.title = article.title
-            item.published = article.date
-            item.updated = article.last_modified
-            item.description = article.to_html
+            item.link = "#{conf.base_url}?date=#{entry.date.strftime('%Y%m%d')}"
+            item.title = entry.title
+            item.published = entry.date
+            item.updated = entry.last_modified
+            item.description = entry.to_enum(:each_section).map {|section| '<div class="section">' << section.body_to_html << '</div>'}.join
           end
         end
       }
 
       Response.new([feed.to_s], 200, {'Content-Type' => 'application/atom+xml'})
+    end
+
+    private
+
+    def entries(limit=DEFAULT_COUNT)
+      entries = []
+      count = 0
+      @io.calendar.reverse_each do |year, months|
+        months.reverse_each do |month|
+          @io.transaction(Date.new(year.to_i, month.to_i)) do |diaries|
+            diaries.reverse_each do |date, entry|
+              if entry.visible? and count < limit
+                entries << entry
+                count += 1
+              end
+              TDiaryBase::DIRTY_DIARY
+            end
+          end
+          break if count >= limit
+        end
+        break if count >= limit
+      end
+      entries
     end
   end
 end
